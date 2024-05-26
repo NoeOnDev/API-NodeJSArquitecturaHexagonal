fa03892de3138367c836294fda053df89a77c8b0
NoeOnDev <alxg5516@gmail.com>
2024-05-26 13:09:53 -0600

    chore: Add .hintrc file and configure TypeScript consistent casing rule

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+process.loadEnvFile();
 const app = (0, express_1.default)();
-const port = 3000;
+const port = process.env.PORT;
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript with Express!');
 });
