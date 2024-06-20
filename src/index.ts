bd5a24612c4dbdcaeacc36acd1dd8c612e0900d9
NoeOnDev <alxg5516@gmail.com>
2024-06-20 14:07:37 -0600

    chore: Update TypeScript configuration and npm scripts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,9 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 
 process.loadEnvFile();
 
 const app = express();
 const port = process.env.PORT;
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello, TypeScript with Express!');
-});
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
