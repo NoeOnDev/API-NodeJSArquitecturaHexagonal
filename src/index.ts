efe35f979c27cd254399f7b81c25b5ee61897654
NoeOnDev <alxg5516@gmail.com>
2024-06-29 15:43:51 -0600

    chore: Update TypeScript configuration and npm scripts

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
 import express from 'express';
 
-process.loadEnvFile();
-
 const app = express();
-const port = process.env.PORT;
 
-app.listen(port, () => {
-    console.log(`Server is running at http://localhost:${port}`);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
