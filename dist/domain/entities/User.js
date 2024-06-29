7ddcf4becf102dd77b3547097c69b55a38107851
NoeOnDev <alxg5516@gmail.com>
2024-06-29 16:06:30 -0600

    feat: Add User entity and UserRepository
    The code changes include adding the User entity and UserRepository files in both the `src/domain/entities` and `src/domain/repositories` directories. These files define the User class and the UserRepository interface respectively. This commit adds the necessary code to implement user-related functionality in the application.

---
+++ dist/domain/entities/User.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.User = void 0;
+class User {
+    constructor(id, username, street, email, password) {
+        this.id = id;
+        this.username = username;
+        this.street = street;
+        this.email = email;
+        this.password = password;
+    }
+}
+exports.User = User;
---
+++ src/domain/repositories/UserRepository.ts
@@ -0,0 +1,10 @@
+import { User } from "../entities/User"
+
+export interface UserRepository {
+    save(user: User): Promise<void>
+    findByEmail(email: string): Promise<User | null>
+    findById(id: string): Promise<User | null>
+    deleteById(id: string): Promise<void>
+    update(user: User): Promise<void>
+    findAll(): Promise<User[]>
+}
